c5313d9d9a88502702e575802cefb1a21340cc77
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-04 09:45:30 +0100

    fetching from server

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -22,10 +23,45 @@ const DUMMY_DATA = [
 ];
 
 export default function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-7d875-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meets = mapResponseToArr(data);
+        setIsLoading(false);
+        setMeetups(meets);
+      });
+  }, []);
+
+  function mapResponseToArr(data) {
+    const meetups = [];
+
+    for (const key in data) {
+      const meetup = {
+        id: key,
+        ...data[key],
+      };
+      meetups.push(meetup);
+    }
+    return meetups;
+  }
+
+  if (isLoading) {
+    return <section>Loading...</section>;
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={meetups} />
     </section>
   );
 }
